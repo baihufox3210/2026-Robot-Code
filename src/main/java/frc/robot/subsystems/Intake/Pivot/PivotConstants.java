580b8b6f07f2c6bd44ca16444b6c2ec77eb8bb1b
baihu <baihu3210@gmail.com>
2026-03-04 17:30:30 +0800

    Update PivotConstants for motor ID and tuning parameters; adjust kP, DownAngle, and tolerance values for improved performance.

--- src/main/java/frc/robot/subsystems/Intake/Pivot/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/Intake/Pivot/PivotConstants.java
@@ -8,17 +8,17 @@ import frc.robot.hardware.Factory.MotorFactory.MotorModel;
 public class PivotConstants {
     public static final MotorModel PivotMotorModel = MotorModel.NeoVortex;
 
-    public static final int PivotMotorID = 30;
+    public static final int PivotMotorID = 31;
 
     public static final double GearRatio = 25 * 34 / 20.0;
 
     public static final double PositionConversionFactor = 2 * Math.PI / GearRatio;
     public static final double VelocityConversionFactor = PositionConversionFactor / 60;
 
-    public static final double kP = 0.22;
+    public static final double kP = 0.25;
     public static final double kV = 0.05;
     public static final double kS = 0.0;
 
-    public static final Angle DownAngle = Degrees.of(90);
-    public static final Angle tolerance = Degrees.of(.5);
+    public static final Angle DownAngle = Degrees.of(95);
+    public static final Angle tolerance = Degrees.of(5);
 }

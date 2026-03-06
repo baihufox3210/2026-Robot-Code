767fae956559add38713a9c5bc384a34ec93e0d4
baihu <baihu3210@gmail.com>
2026-03-06 16:19:48 +0800

    Refactor Shooting command to include Feeder dependency for coordinated operation

--- src/main/java/frc/robot/commands/Shooter/Shooting.java
+++ src/main/java/frc/robot/commands/Shooter/Shooting.java
@@ -1,23 +1,29 @@
 package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake.Feeder.Feeder;
 import frc.robot.subsystems.Shooter.Shooter;
 
 public class Shooting extends Command {
     private final Shooter shooter;
+    private final Feeder feeder;
 
     public Shooting() {
         shooter = Shooter.getInstance();
-        addRequirements(shooter);
+        feeder = Feeder.getInstance();
+
+        addRequirements(shooter, feeder);
     }
 
     @Override
     public void execute() {
         shooter.run();
+        feeder.run();
     }
 
     @Override
     public void end(boolean interrupted) {
         shooter.stop();
+        feeder.stop();
     }
 }

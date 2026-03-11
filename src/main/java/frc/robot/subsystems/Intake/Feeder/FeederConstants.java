a7e9a7d84ec44700c23b25e6115a08764efffb91
baihu <baihu3210@gmail.com>
2026-03-11 19:59:45 +0800

    fix: Reduce shooting speed

--- src/main/java/frc/robot/subsystems/Intake/Feeder/FeederConstants.java
+++ src/main/java/frc/robot/subsystems/Intake/Feeder/FeederConstants.java
@@ -7,5 +7,5 @@ public class FeederConstants {
 
     public static final int FeederMotorID = 33;
 
-    public static final double ShootingSpeed = 0.7;
+    public static final double ShootingSpeed = 0.3;
 }

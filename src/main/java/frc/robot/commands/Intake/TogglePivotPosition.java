bc9ebf538cd9ff7ae7acdd77855f1124f190f6b3
baihu <baihu3210@gmail.com>
2026-03-12 11:12:47 +0800

    refactor: Refactor: Pivot position toggle command

--- src/main/java/frc/robot/commands/Intake/TogglePivotPosition.java
+++ src/main/java/frc/robot/commands/Intake/TogglePivotPosition.java
@@ -6,32 +6,48 @@ import frc.robot.subsystems.Intake.Pivot.Pivot.PivotMode;
 
 public class TogglePivotPosition extends Command {
     private final Pivot pivot;
-
-    private boolean isDown = false;
-    private PivotMode targetMode; 
+    private Command command;
 
     public TogglePivotPosition() {
         pivot = Pivot.getInstance();
-        addRequirements(pivot);
     }
 
     @Override
     public void initialize() {
-        if(isDown) targetMode = PivotMode.RETRACT;
-        else targetMode = PivotMode.DOWN;
+        if(pivot.getPivotState() == PivotMode.RETRACT) command = new setPivotPosition(PivotMode.DOWN.getPosition());
+        else command = new setPivotPosition(PivotMode.RETRACT.getPosition());
 
-        pivot.setPosition(targetMode.getPosition());
+        pivot.togglePivotState();
 
-        isDown = !isDown;
+        command.schedule();
     }
 
     @Override
     public boolean isFinished() {
-        return pivot.isPivotAtPosition(targetMode.getPosition());
+        return true;
     }
 
-    @Override
-    public void end(boolean interrupted) {
-        pivot.stop();
+    private class setPivotPosition extends Command {
+        private double position;
+
+        public setPivotPosition(double position) {
+            this.position = position;
+            addRequirements(pivot);
+        }
+
+        @Override
+        public void initialize() {
+            pivot.setPosition(position);
+        }
+
+        @Override
+        public boolean isFinished() {
+            return pivot.isPivotAtPosition(position);
+        }
+
+        @Override 
+        public void end(boolean interrupted) {
+            if(!interrupted) pivot.stop();
+        }
     }
 }

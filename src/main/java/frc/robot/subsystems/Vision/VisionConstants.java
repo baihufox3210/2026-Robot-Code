c32acd4b239ba14aa615305101bd267dccc1fbb7
baihu <baihu3210@gmail.com>
2026-03-13 15:17:57 +0800

    fix: Update camera name in VisionConstants

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -2,14 +2,19 @@ package frc.robot.subsystems.Vision;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 
 public class VisionConstants {
-    public static final String CameraName = "";
+    public static final String CameraName = "HD_Pro_Webcam_C920 (1)";
     
-    public static final Transform3d RobotToCamera = new Transform3d();
+    public static final Transform3d RobotToCamera = new Transform3d(
+        new Translation3d(0.0, 0.0, 0.0),
+        new Rotation3d(0.0, 0.0, 0.0)
+    );
 
     public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
     public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);

3958a117340dfb78e2558621e3b0250e3f43a270
baihu <baihu3210@gmail.com>
2026-03-13 15:11:05 +0800

    feat: Add field pose transformation

--- src/main/java/frc/robot/utils/FieldTransformer.java
+++ src/main/java/frc/robot/utils/FieldTransformer.java
@@ -3,9 +3,22 @@ package frc.robot.utils;
 import frc.robot.RobotConstants;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class FieldTransformer {
     private FieldTransformer() {}
+
+    public static Translation2d transform(Translation2d blueAlliancePose) {
+        if(RobotConstants.isRedAlliance()) return mirror(blueAlliancePose);
+        return blueAlliancePose;
+    }
+
+    private static Translation2d mirror(Translation2d pose) {
+        return new Translation2d(
+            RobotConstants.FieldSize.getX() - pose.getX(),
+            pose.getY()
+        );
+    }
 
     public static Pose2d transform(Pose2d blueAlliancePose) {
         if(RobotConstants.isRedAlliance()) return mirror(blueAlliancePose);

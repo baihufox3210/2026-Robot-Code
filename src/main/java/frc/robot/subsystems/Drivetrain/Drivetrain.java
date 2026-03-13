c55580d7ceb0395507933cc0317645d27680ccf1
baihu <baihu3210@gmail.com>
2026-03-13 17:14:40 +0800

    refactor: Update AimingParameters class

--- src/main/java/frc/robot/subsystems/Drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/Drivetrain.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.config.RobotConfig;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -23,7 +24,7 @@ import frc.robot.hardware.interfaces.GenericGyro;
 import frc.robot.subsystems.Drivetrain.DrivetrainConstants.DriveMotorConfig;
 import frc.robot.subsystems.Drivetrain.DrivetrainConstants.StreetMotorConfig;
 import frc.robot.subsystems.Drivetrain.module.SwerveModule;
-import frc.robot.utils.AutoAimCalculator;
+import frc.robot.utils.AimingParameters;
 
 public class Drivetrain extends SubsystemBase {
     private static Drivetrain instance;
@@ -87,25 +88,29 @@ public class Drivetrain extends SubsystemBase {
         poseEstimator.resetPosition(gyro.getRotation2d(), getModulePositions(), pose);
     }
 
+    public Rotation2d getHeading() {
+        return gyro.getRotation2d();
+    }
+
     private SwerveModulePosition[] getModulePositions() {
         SwerveModulePosition[] positions = new SwerveModulePosition[4];
         for (int i = 0; i < 4; i++) positions[i] = swerveModules[i].getPosition();
         return positions;
     }
 
-    private void driveToAngle(double xSpeed, double ySpeed, double targetAngle) {
-        double rotationOutput = DrivetrainConstants.rotationPID.calculate(
-            gyro.getRotation2d().getRadians(),
-            targetAngle
-        );
+    // private void driveToAngle(double xSpeed, double ySpeed, double targetAngle) {
+    //     double rotationOutput = DrivetrainConstants.rotationPID.calculate(
+    //         gyro.getRotation2d().getRadians(),
+    //         targetAngle
+    //     );
 
-        drive(xSpeed, ySpeed, rotationOutput);
-    }
+    //     drive(xSpeed, ySpeed, rotationOutput);
+    // }
 
-    public void driveAutoAim(double xSpeed, double ySpeed) {
-        double targetRadians = AutoAimCalculator.getTargetHeading(getPose());
-        driveToAngle(xSpeed, ySpeed, targetRadians);
-    }
+    // public void driveAutoAim(double xSpeed, double ySpeed) {
+    //     double targetRadians = AimingParameters.getTargetHeading(getPose());
+    //     driveToAngle(xSpeed, ySpeed, targetRadians);
+    // }
 
     public void drive(double xSpeed, double ySpeed, double rot) {
         ChassisSpeeds chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(

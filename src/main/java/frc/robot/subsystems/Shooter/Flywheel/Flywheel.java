b7f7f726714c186f594d90c0c79884b8c51428ae
baihu <baihu3210@gmail.com>
2026-03-14 21:45:02 +0800

    fix: Update flywheel velocity display

--- src/main/java/frc/robot/subsystems/Shooter/Flywheel/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Shooter/Flywheel/Flywheel.java
@@ -1,18 +1,30 @@
 package frc.robot.subsystems.Shooter.Flywheel;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.hardware.Factory.MotorFactory;
+import frc.robot.hardware.interfaces.GenericEncoder;
 import frc.robot.hardware.interfaces.GenericMotor;
 
 public class Flywheel extends SubsystemBase {
     private static Flywheel instance;
 
     private final GenericMotor flywheelMotor;
+    private final GenericEncoder flywheelEncoder;
 
     private Flywheel() {
-        flywheelMotor = MotorFactory.createMotor(FlywheelConstants.FlywheelMotorID, FlywheelConstants.FlywheelMotorModel, FlywheelConfig.getFlywheelMotorConfig());
+        flywheelMotor = MotorFactory.createMotor(FlywheelConstants.FlywheelMotorID, FlywheelConstants.FlywheelMotorModel, FlywheelConfig.getFlywheelMotorConfig());        
         flywheelMotor.configure();
+
+        flywheelEncoder = flywheelMotor.getEncoder();
     }
+
+    @Override
+    public void periodic() {
+        double currentVelocity = flywheelEncoder.getVelocity();
+        SmartDashboard.putNumber("FLywheel/Current Veloctiy", currentVelocity);
+    }
+
 
     private void setVelocity(double speed) {
         flywheelMotor.setVelocity(speed);
